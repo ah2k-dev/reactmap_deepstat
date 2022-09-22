7495e6eff5a9396355e1eaba755d54b13bbbf88b
Ahmed Hassan <ah2k.dev@gmail.com>
2022-09-22 16:44:58 +0500

    updates

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,21 @@
-import React, { useEffect, useRef } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useEffect, useRef, useState } from "react";
+import { MapContainer, TileLayer, Polygon } from "react-leaflet";
 import RecenterAutomatically from "./Receneter";
-import {HeatmapLayer} from "react-leaflet-heatmap-layer-v3";
+import { HeatmapLayer } from "react-leaflet-heatmap-layer-v3";
 import { data } from "./geoJson";
 const Map = ({ position, zoom }) => {
+  const [coOrdinates, setCoOrdinates] = useState([]);
+  const setCoordinates = () => {
+    const coordinates = data.features.map((feature) => {
+      return feature.geometry.coordinates;
+    });
+    setCoOrdinates(coordinates);
+    console.log(coordinates);
+  };
+  useEffect(() => {
+    setCoordinates();
+  }, [])
+  
   const ref = useRef();
   return (
     <MapContainer
@@ -12,18 +24,19 @@ const Map = ({ position, zoom }) => {
       //   scrollWheelZoom={true}
       style={{ width: "100vw", height: "500px" }}
     >
-        <HeatmapLayer
-          fitBoundsOnLoad
-          fitBoundsOnUpdate
-          points={data.features}
-          longitudeExtractor={(m) => m.geometry.coordinates[1]}
-          latitudeExtractor={(m) => m.geometry.coordinates[0]}
-          intensityExtractor={(m) => parseFloat(m.geometry.coordinates[2])}
-        />
+      <HeatmapLayer
+        fitBoundsOnLoad
+        fitBoundsOnUpdate
+        points={data.features}
+        longitudeExtractor={(m) => m.geometry.coordinates[1]}
+        latitudeExtractor={(m) => m.geometry.coordinates[0]}
+        intensityExtractor={(m) => parseFloat(m.geometry.coordinates[1])}
+      />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {/* <Polygon positions={[position]} /> */}
       {/* <Marker position={position}></Marker> */}
       <RecenterAutomatically
         lat={position[0]}
--- src/geoJson.js
+++ src/geoJson.js
@@ -1,46 +1,53 @@
 export const data = {
-  type: "FeatureCollection",
+//   type: "FeatureCollection",
   features: [
     {
-      type: "Feature",
+    //   type: "Feature",
       geometry: {
-        type: "Point",
-        coordinates: [24.891664396702538, 67.08112309276848, 95],
+        // type: "Point",
+        coordinates: [24.891664396702538, 67.08112309276848, 1],
       },
     },
     {
-      type: "Feature",
+        // type: "Feature",
+        geometry: {
+        //   type: "Point",
+          coordinates: [24.891, 67.081, 1],
+        },
+      },
+    {
+    //   type: "Feature",
       geometry: {
-        type: "Point",
-        coordinates: [24.87484538045037, 67.09743077324872, 145],
+        // type: "Point",
+        coordinates: [24.87484538045037, 67.09743077324872, 1],
       },
     },
     {
-      type: "Feature",
+    //   type: "Feature",
       geometry: {
-        type: "Point",
-        coordinates: [24.882205601533492, 67.10722444746591, 200],
+        // type: "Point",
+        coordinates: [24.882205601533492, 67.10722444746591, 1],
       },
     },
     {
-      type: "Feature",
+    //   type: "Feature",
       geometry: {
-        type: "Point",
-        coordinates: [24.857967789752806, 67.06066103173531, 250],
+        // type: "Point",
+        coordinates: [24.857967789752806, 67.06066103173531, 1],
       },
     },
     {
-      type: "Feature",
+    //   type: "Feature",
       geometry: {
-        type: "Point",
-        coordinates: [24.861569587298533, 67.05964689387258, 400],
+        // type: "Point",
+        coordinates: [24.861569587298533, 67.05964689387258, 1],
       },
     },
     {
-      type: "Feature",
+    //   type: "Feature",
       geometry: {
-        type: "Point",
-        coordinates: [24.868536270371845, 67.08230563132842, 300],
+        // type: "Point",
+        coordinates: [24.868536270371845, 67.08230563132842, 2],
       },
     },
   ],
